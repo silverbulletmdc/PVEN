a0ef87d45306352dded27dbc03e31915714b1a81
Dechao Meng <dechao.meng@vipl.ict.ac.cn>
2020-08-08 12:48:29 +0800

    add init

---
+++ vehicle_reid_pytorch/data/datasets/__init__.py
@@ -0,0 +1,5 @@
+from .bases import ReIDDataset, ReIDMetaDataset
+from .veri776 import VeRi776
+from .vehicleid import VehicleID
+from .veriwild import VeriWild
+from .common import CommonReIDDataset
