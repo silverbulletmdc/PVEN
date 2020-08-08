d58063285ac6a3b7e04d8ca0797f35cb79d8e2b8
Dechao Meng <dechao.meng@vipl.ict.ac.cn>
2020-08-08 00:51:40 -0400

    fix bug

--- vehicle_reid_pytorch/data/datasets/__init__.py
+++ vehicle_reid_pytorch/data/datasets/__init__.py
@@ -1,5 +1,2 @@
 from .bases import ReIDDataset, ReIDMetaDataset
-from .veri776 import VeRi776
-from .vehicleid import VehicleID
-from .veriwild import VeriWild
 from .common import CommonReIDDataset
